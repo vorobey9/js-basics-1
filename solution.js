64cfc027fbd2993bece63d92ce2970d3dd194a8a
Andrey Bakuta <anb@anahoret.com>
2014-12-18 17:31:37 +0200

    Remove rather misleading hint.

--- solution.js
+++ solution.js
@@ -13,19 +13,13 @@ function drawTriangle() {
   // #####
   // ######
   // #######
-  //
-  // It may be useful to know that you can find the length of a string by writing .length after it.
-  //
-  // var abc = "abc";
-  // console.log(abc.length);
-  // // → 3
 }
 
 function fizzBuzz() {
   // Write a program that uses console.log to print all the numbers from 1 to 100,
   // with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number,
   // and for numbers divisible by 5 (and not 3), print "Buzz" instead.
-
+  //
   // When you have that working, modify your program to print "FizzBuzz",
   // for numbers that are divisible by both 3 and 5 (and still print "Fizz"
   // or "Buzz" for numbers divisible by only one of those).
