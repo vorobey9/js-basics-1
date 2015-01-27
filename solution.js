51c35b83db6cd17e5017112a2d9237e4bab204d3
dra1n <dra1n86@gmail.com>
2015-01-27 09:06:41 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -26,7 +26,7 @@ function fizzBuzz() {
 }
 
 function chessBoard(size) {
-  // Write a program that creates a string that represents an 8×8 grid,
+  // Write a program that returns a string that represents an 8×8 grid,
   // using newline characters to separate lines. At each position of the
   // grid there is either a space or a “#” character. The characters
   // should form a chess board.
